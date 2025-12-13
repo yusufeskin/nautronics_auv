559f981226f6cf416d3a93664aacc8eed8b6e7f4
Sami Yusuf Eskin <yusuf.eskin@metu.edu.tr>
2025-12-14 01:28:01 +0300

    Feat: rviz opening and ros bridge added
    - to visualize data, rviz config file created and added to the auv_description package
    - launch arguments added to the launch file (more of them will needed right now it seems ok)
    - ros_gz_bridge config file added to the auv_description and path added to the CMakeLists file
    - some minor adjustments done in xacro files

--- auv_description/launch/gazebo.launch.py
+++ auv_description/launch/gazebo.launch.py
@@ -1,21 +1,39 @@
 import os
 from ament_index_python.packages import get_package_share_directory
 from launch import LaunchDescription
-from launch.actions import IncludeLaunchDescription, SetEnvironmentVariable
+from launch.actions import IncludeLaunchDescription, DeclareLaunchArgument
 from launch.launch_description_sources import PythonLaunchDescriptionSource
 from launch_ros.actions import Node
-from launch.substitutions import Command
+from launch.substitutions import Command, LaunchConfiguration
+from launch.conditions import IfCondition
 
 def generate_launch_description():
     pkg_name = 'auv_description'
     pkg_share = get_package_share_directory(pkg_name)
-
+    
     # 1. Dosya Yolları
     xacro_file = os.path.join(pkg_share, 'models', 'prototype_vehicle', 'prototype.urdf.xacro')
     world_file = os.path.join(pkg_share, 'worlds', 'deneme.world')
-
+    bridge_config_path = os.path.join(pkg_share, 'config', 'bridge.yaml')
+    rviz_config_path = os.path.join(pkg_share, 'config', 'rviz_config.rviz')
     # 2. Robot Tanımı (Xacro -> URDF)
     robot_desc_content = Command(['xacro ', xacro_file])
+
+
+    rviz_arg = DeclareLaunchArgument(
+        'rviz', 
+        default_value='true',
+        description='RViz2 ifcond'
+    )
+
+    sim_time_arg = DeclareLaunchArgument(
+        'use_sim_time', 
+        default_value='true',
+        description='Gazebo time'
+    )
+
+    use_rviz = LaunchConfiguration('rviz')
+    use_sim_time = LaunchConfiguration('use_sim_time')
 
     # 3. Robot State Publisher
     robot_state_publisher = Node(
@@ -29,26 +47,16 @@ def generate_launch_description():
         }]
     )
 
-    # 4. KÖPRÜ (BRIDGE) - Thruster ve Sensor Verileri İçin
-    bridge_arguments = [
-        '/clock@rosgraph_msgs/msg/Clock[gz.msgs.Clock',
-        '/model/prototype_vehicle/joint_state@sensor_msgs/msg/JointState[gz.msgs.Model',
-    ]
-
-    # Thruster Komut Köprüleri (8 Adet)
-    for i in range(1, 9):
-        gz_topic = f'/model/prototype/joint/thruster{i}_joint/cmd_thrust'
-        bridge_arguments.append(f'{gz_topic}@std_msgs/msg/Float64]gz.msgs.Double')
-
-    bridge = Node(
+    ros_gz_bridge = Node(
         package='ros_gz_bridge',
         executable='parameter_bridge',
-        arguments=bridge_arguments,
+        name='ros_gz_bridge',
         output='screen',
-        remappings=[
-            ('/model/prototype_vehicle/joint_state', '/joint_states'),
-        ] + [(f'/model/prototype/joint/thruster{i}_joint/cmd_thrust', f'/thruster/id{i}/cmd') for i in range(1, 9)]
+        parameters=[{
+            'config_file': bridge_config_path,
+        }]
     )
+
 
     # 5. Gazebo Simülasyonu
     # Not: GZ_SIM_RESOURCE_PATH artık setup.bash tarafından otomatik ayarlanıyor!
@@ -67,9 +75,21 @@ def generate_launch_description():
         output='screen'
     )
 
+    rviz_node = Node(
+        package='rviz2',
+        executable='rviz2',
+        name='rviz2',
+        output='screen',
+        arguments=['-d', rviz_config_path],
+        condition=IfCondition(use_rviz)
+    )
+
     return LaunchDescription([
+        rviz_arg,      
+        sim_time_arg,
         robot_state_publisher,
-        bridge,
+        ros_gz_bridge,
         gz_sim,
         spawn_entity,
+        rviz_node
     ])

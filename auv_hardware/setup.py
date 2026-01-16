5edc134793b93532a5577b693ad6527d355d9213
Sami Yusuf Eskin <yusuf.eskin@metu.edu.tr>
2026-01-16 23:08:04 +0300

    Feat: auv_interfaces package added and some minor changes  toauv_hardware - I've created auv_interfaces to write custom message and services - A new node added to auv_hardware in order to publish vehicle's state(such as is_armed, flight mode) - Some unused files and folders deleted -  New srv file added to custom flight mode changer service(client and server will be added after)

--- auv_hardware/setup.py
+++ auv_hardware/setup.py
@@ -30,6 +30,7 @@ setup(
             'pwm_router_node = auv_hardware.pwm_router:main',
             'pixhawk_baro_reader = auv_hardware.baro_publisher:main',
             'ping_sonar_node = auv_hardware.ping_sonar:main',
+            'state_publisher = auv_hardware.state_publisher:main'
         ],
     },
 )

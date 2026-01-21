56474b5f00f65fde5002956f630960dcd47d62f3
Sami Yusuf Eskin <yusuf.eskin@metu.edu.tr>
2026-01-21 18:36:06 +0300

    Feat: basic target point based controller added - this controller subscribes to the /target_point topic and sends the values to ardusub controller - we should convert enu to ned, i will add it -  its for the test, features must be improved

---
+++ auv_control/setup.py
@@ -0,0 +1,30 @@
+from setuptools import find_packages, setup
+
+package_name = 'auv_control'
+
+setup(
+    name=package_name,
+    version='0.0.0',
+    packages=find_packages(exclude=['test']),
+    data_files=[
+        ('share/ament_index/resource_index/packages',
+            ['resource/' + package_name]),
+        ('share/' + package_name, ['package.xml']),
+    ],
+    install_requires=['setuptools'],
+    zip_safe=True,
+    maintainer='sye',
+    maintainer_email='yusuf.eskin@metu.edu.tr',
+    description='TODO: Package description',
+    license='TODO: License declaration',
+    extras_require={
+        'test': [
+            'pytest',
+        ],
+    },
+    entry_points={
+        'console_scripts': [
+            'point_follower = auv_control.point_follower:main',
+        ],
+    },
+)

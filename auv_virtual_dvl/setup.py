3037f350eec73cf4c29f8cc823b153ccdf06e1cc
akkanmete <metteakkan@gmail.com>
2026-04-28 20:37:33 +0300

    opt flow and pwm publisheradded, launch file updated, setup.py updated

--- auv_virtual_dvl/setup.py
+++ auv_virtual_dvl/setup.py
@@ -1,3 +1,5 @@
+import os
+from glob import glob
 from setuptools import find_packages, setup
 
 package_name = 'auv_virtual_dvl'
@@ -6,14 +8,16 @@ setup(
     name=package_name,
     version='0.0.0',
     packages=[
-    'auv_virtual_dvl',
-    'auv_virtual_dvl.models',
-    'auv_virtual_dvl.scripts'
-],
+        'auv_virtual_dvl',
+        'auv_virtual_dvl.models',
+        'auv_virtual_dvl.scripts'
+    ],
     data_files=[
         ('share/ament_index/resource_index/packages',
             ['resource/' + package_name]),
         ('share/' + package_name, ['package.xml']),
+        # LAUNCH KLASÖRÜNÜ TANITAN SATIR:
+        (os.path.join('share', package_name, 'launch'), glob(os.path.join('launch', '*launch.[pxy][yma]*'))),
     ],
     install_requires=['setuptools'],
     zip_safe=True,
@@ -29,6 +33,8 @@ setup(
     entry_points={
         'console_scripts': [
             'dvl_calistir = auv_virtual_dvl.realtime_node:main',
+            'metrekayma = auv_virtual_dvl.metrekayma:main',
+            'pwm_publisher = auv_virtual_dvl.pwm_publisher:main'
         ],
     },
 )

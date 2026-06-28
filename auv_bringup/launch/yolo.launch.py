c7dfd2a321ec7049e32417b89d65c8c227ee8372
Sami Yusuf Eskin <yusuf.eskin@metu.edu.tr>
2026-06-29 01:58:34 +0300

    parameters adjusted, launcher added,

---
+++ auv_bringup/launch/yolo.launch.py
@@ -0,0 +1,43 @@
+import os
+from launch import LaunchDescription
+from launch_ros.actions import Node
+
+def generate_launch_description():
+    
+    yolo_node = Node(
+        package='auv_vision',            
+        executable='yolo_keypoint_lifecycle', 
+        name='universal_yolo_node', 
+        output='screen',
+        emulate_tty=True,         
+        parameters=[
+            {
+                'model_name': 'slalom.pt', 
+                'model_type': 'bbox',    
+                'image_topic': '/camera/front',      
+                'ema_alpha': 0.70,                 
+                'distance_gate_threshold': 40.0,   
+                'miss_frames_limit': 15            
+            }
+        ]
+    )
+    
+    bbox_pnp_solver = Node(
+        package='auv_vision',
+        executable='bbox_pnp_solver',
+        name='bbox_pnp_solver_node',
+        output='screen',
+        emulate_tty=True,
+        parameters=[
+            {
+                'info_topic': '/camera/camera_info'
+            }
+        ]
+    
+    )
+
+
+    return LaunchDescription([
+        yolo_node,
+        bbox_pnp_solver
+    ])

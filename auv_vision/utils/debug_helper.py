f19f6f972fd598b118e07fe4fdd445dafa107ad7
Sami Yusuf Eskin <yusuf.eskin@metu.edu.tr>
2026-07-02 13:30:44 +0300

    minor changes for tracker

--- auv_vision/utils/debug_helper.py
+++ auv_vision/utils/debug_helper.py
@@ -4,7 +4,6 @@ import numpy as np
 from sensor_msgs.msg import CompressedImage
 from auv_interfaces.msg import DetectionArray
 
-
 def draw_debug(frame, results, detections_msg: DetectionArray, model_type: str) -> np.ndarray:
     debug_frame = frame.copy()
     r = results[0]
@@ -13,7 +12,13 @@ def draw_debug(frame, results, detections_msg: DetectionArray, model_type: str) 
     if model_type in ('bbox', 'keypoint'):
         for i, box in enumerate(r.boxes):
             x1, y1, x2, y2 = map(int, box.xyxy[0])
-            label = f"{dets[i].class_name} {dets[i].confidence:.2f}"
+            
+            track_id = dets[i].tracking_id
+            if track_id != -1:
+                label = f"ID:{track_id} {dets[i].class_name} {dets[i].confidence:.2f}"
+            else:
+                label = f"{dets[i].class_name} {dets[i].confidence:.2f}"
+            
             cv2.rectangle(debug_frame, (x1, y1), (x2, y2), (255, 0, 0), 2)
             cv2.putText(debug_frame, label, (x1, y1 - 10),
                         cv2.FONT_HERSHEY_SIMPLEX, 0.5, (255, 0, 0), 2)
@@ -24,6 +29,7 @@ def draw_debug(frame, results, detections_msg: DetectionArray, model_type: str) 
                     cx, cy = int(det.keypoints[idx].x), int(det.keypoints[idx].y)
                     if cx == 0 and cy == 0:
                         continue  # skip uninitialised / zero keypoints
+                    
                     cv2.circle(debug_frame, (cx, cy), 5, (0, 255, 0), -1)
                     cv2.putText(debug_frame, str(idx), (cx + 5, cy - 5),
                                 cv2.FONT_HERSHEY_SIMPLEX, 0.5, (0, 0, 255), 1)
@@ -34,7 +40,13 @@ def draw_debug(frame, results, detections_msg: DetectionArray, model_type: str) 
                 pts = det.xyxyxyxy[0].cpu().numpy().astype(int)
                 cv2.polylines(debug_frame, [pts], isClosed=True,
                               color=(0, 255, 255), thickness=2)
-                label = f"{dets[i].class_name} {dets[i].confidence:.2f}"
+                
+                track_id = dets[i].tracking_id
+                if track_id != -1:
+                    label = f"ID:{track_id} {dets[i].class_name} {dets[i].confidence:.2f}"
+                else:
+                    label = f"{dets[i].class_name} {dets[i].confidence:.2f}"
+                    
                 cv2.putText(debug_frame, label, (pts[0][0], pts[0][1] - 10),
                             cv2.FONT_HERSHEY_SIMPLEX, 0.5, (0, 255, 255), 2)
 

e9e43dde3d6474f6ab09fcff339e813b345ee3ca
Mehmet Barış Gül <mehmetbgul58@gmail.com>
2026-07-05 22:00:49 +0300

    feat: checkers added for mode change.

--- auv_bt/pool/teknofest.py
+++ auv_bt/pool/teknofest.py
@@ -18,6 +18,8 @@ import py_trees.timers
 import common_behaviors.state 
 import gate.behaviours.depth
 import gate.behaviours.arrange_depth_action
+import behaviours.attitude
+import behaviours.depth
 
 from auv_interfaces.action import BlindPush
 from auv_interfaces.action import YawAndScan
@@ -27,8 +29,9 @@ from auv_interfaces.action import ReturnLoop
 
 def create_root() -> py_trees.behaviour.Behaviour:
 
-# 1. MAIN TREE STRUCTURE
-
+    # ==========================================
+    # 1. MAIN TREE STRUCTURE & ROOT
+    # ==========================================
     root = py_trees.composites.Parallel(
         name="Main Parallel Root",
         policy=py_trees.common.ParallelPolicy.SuccessOnAll(synchronise=False)
@@ -47,8 +50,10 @@ def create_root() -> py_trees.behaviour.Behaviour:
         policy=py_trees.common.OneShotPolicy.ON_SUCCESSFUL_COMPLETION
     )
 
-# 2. PUBLISHERS BRANCH
 
+    # ==========================================
+    # 2. PUBLISHERS BRANCH
+    # ==========================================
     mode2bb = common_behaviors.state.ToBlackboard(
         name="Mode2BB",
         topic_name="/vehicle/state",
@@ -63,12 +68,15 @@ def create_root() -> py_trees.behaviour.Behaviour:
 
     publishers_parallel.add_children([mode2bb, depth2bb])
 
-# 3. ARRANGE DEPTH BRANCH
 
-    arrange_depth_sequence = py_trees.composites.Sequence("Arrange Depth", memory=True)
-
+    # ==========================================
+    # 3. FIRST PART (INITIAL DEPTH & PUSH)
+    # ==========================================
     wait_60_secs1 = py_trees.timers.Timer(name="Wait 60 Seconds", duration=35.0)
 
+    # ------------------------------------------
+    # Action + Check: Switch to ALT_HOLD 1
+    # ------------------------------------------
     mode_request_althold1 = SetVehicleMode.Request()
     mode_request_althold1.mode_name = "ALT_HOLD"
     switch_mode_althold1 = py_trees_ros.service_clients.FromConstant(
@@ -76,8 +84,35 @@ def create_root() -> py_trees.behaviour.Behaviour:
         service_type=SetVehicleMode,
         service_name="/change_mode",
         service_request=mode_request_althold1
-        )
+    )
     
+    wait_mode_althold1 = py_trees.decorators.Timeout(
+        name="Timeout Wait Mode ALT_HOLD 1",
+        duration=2.0,
+        child=py_trees.behaviours.WaitForBlackboardVariableValue(
+            name="Wait Mode ALT_HOLD 1",
+            check=py_trees.common.ComparisonExpression(
+                variable="vehicle_mode",
+                value="ALT_HOLD",
+                operator=operator.eq)
+        )
+    )
+
+    retry_switch_althold1 = py_trees.composites.Sequence(
+        name="Seq Switch AltHold 1",
+        memory=True,
+        children=[switch_mode_althold1, wait_mode_althold1]
+    )
+
+    retry_switch_althold1 = py_trees.decorators.Retry(
+        name="Retry Switch Althold1",
+        child=retry_switch_althold1,
+        num_failures=10
+    )
+
+    # ------------------------------------------
+    # Action: Arrange Depth 1
+    # ------------------------------------------
     arrange_depth1 = gate.behaviours.arrange_depth_action.ArrangeDepthAction(
         name="Arrange Depth",
         topic_odom="/baro_data",
@@ -86,7 +121,10 @@ def create_root() -> py_trees.behaviour.Behaviour:
         tolerance=0.1,   
         speed=0.4             
     )
-    
+
+    # ------------------------------------------
+    # Action: Blind Push 1
+    # ------------------------------------------
     blind_push1 = py_trees_ros.action_clients.FromConstant(
         name="Blind Push Through Gate",
         action_type=BlindPush,
@@ -97,40 +135,50 @@ def create_root() -> py_trees.behaviour.Behaviour:
         )
     )
 
-    goal_msg1 = YawAndScan.Goal()
-    goal_msg1.target_angle_deg = 180.0
-    goal_msg1.angular_speed = 0.05 
-
-    rotate_180_deg = py_trees_ros.action_clients.FromConstant(
-        name="Turn 180 degrees",
-        action_type=YawAndScan,
-        action_name="/yaw_and_scan",
-        action_goal=goal_msg1
-    )
-
-    blind_push2 = py_trees_ros.action_clients.FromConstant(
-        name="Blind Push Through Gate",
-        action_type=BlindPush,
-        action_name="/blind_push",
-        action_goal=BlindPush.Goal(
-            duration=3.0,
-            speed=0.2
-        )
-    )
-
+    # ------------------------------------------
+    # Action + Check: Switch to MANUAL 1
+    # ------------------------------------------
     mode_request_manual1 = SetVehicleMode.Request()
     mode_request_manual1.mode_name = "MANUAL"
     switch_mode_manual1 = py_trees_ros.service_clients.FromConstant(
-            name="SwitchToManual",
-            service_type=SetVehicleMode,
-            service_name="/change_mode",
-            service_request=mode_request_manual1
+        name="SwitchToManual",
+        service_type=SetVehicleMode,
+        service_name="/change_mode",
+        service_request=mode_request_manual1
+    )
+
+    wait_mode_manual1 = py_trees.decorators.Timeout(
+        name="Timeout Wait Mode MANUAL 1",
+        duration=2.0,
+        child=py_trees.behaviours.WaitForBlackboardVariableValue(
+            name="Wait Mode MANUAL 1",
+            check=py_trees.common.ComparisonExpression(
+                variable="vehicle_mode",
+                value="MANUAL",
+                operator=operator.eq)
         )
+    )
+
+    retry_switch_manual1 = py_trees.composites.Sequence(
+        name="Seq Switch Manual 1",
+        memory=True,
+        children=[switch_mode_manual1, wait_mode_manual1]
+    )
+
+    retry_switch_manual1 = py_trees.decorators.Retry(
+        name="Retry Switch Manual1",
+        child=retry_switch_manual1,
+        num_failures=10
+    )
     
-
-
+    # ==========================================
+    # 4. SECOND PART (SQUARE PATH & LOOP)
+    # ==========================================
     wait_60_secs2 = py_trees.timers.Timer(name="Wait 60 Seconds", duration=20.0)
 
+    # ------------------------------------------
+    # Action + Check: Switch to ALT_HOLD 2
+    # ------------------------------------------
     mode_request_althold2 = SetVehicleMode.Request()
     mode_request_althold2.mode_name = "ALT_HOLD"
     switch_mode_althold2 = py_trees_ros.service_clients.FromConstant(
@@ -138,8 +186,35 @@ def create_root() -> py_trees.behaviour.Behaviour:
         service_type=SetVehicleMode,
         service_name="/change_mode",
         service_request=mode_request_althold2
-        )
+    )
 
+    wait_mode_althold2 = py_trees.decorators.Timeout(
+        name="Timeout Wait Mode ALT_HOLD 2",
+        duration=2.0,
+        child=py_trees.behaviours.WaitForBlackboardVariableValue(
+            name="Wait Mode ALT_HOLD 2",
+            check=py_trees.common.ComparisonExpression(
+                variable="vehicle_mode",
+                value="ALT_HOLD",
+                operator=operator.eq)
+        )
+    )
+
+    retry_switch_althold2 = py_trees.composites.Sequence(
+        name="Seq Switch AltHold 2",
+        memory=True,
+        children=[switch_mode_althold2, wait_mode_althold2]
+    )
+
+    retry_switch_althold2 = py_trees.decorators.Retry(
+        name="Retry Switch Althold2",
+        child=retry_switch_althold2,
+        num_failures=10
+    )
+
+    # ------------------------------------------
+    # Action: Arrange Depth 2
+    # ------------------------------------------
     arrange_depth2 = gate.behaviours.arrange_depth_action.ArrangeDepthAction(
         name="Arrange Depth",
         topic_odom="/baro_data",
@@ -149,97 +224,130 @@ def create_root() -> py_trees.behaviour.Behaviour:
         speed=0.4             
     )
 
-    blind_push3 = py_trees_ros.action_clients.FromConstant(
+
+    # --- SHARED GOALS FOR EDGES ---
+    goal_msg = YawAndScan.Goal()
+    goal_msg.target_angle_deg = 90.0
+    goal_msg.angular_speed = 0.05 
+
+
+    # --- First Edge ---
+    blind_push2 = py_trees_ros.action_clients.FromConstant(
         name="Blind Push Through Gate",
         action_type=BlindPush,
         action_name="/blind_push",
-        action_goal=BlindPush.Goal(
-            duration=17.0,
-            speed=0.2
-        )
+        action_goal=BlindPush.Goal(duration=17.0, speed=0.2)
     )
 
-    goal_msg2 = YawAndScan.Goal()
-    goal_msg2.target_angle_deg = 90.0
-    goal_msg2.angular_speed = 0.05 
-    
+    # ------------------------------------------
+    # Action: Rotate 90 Deg 1
+    # ------------------------------------------
     rotate_90_deg1 = py_trees_ros.action_clients.FromConstant(
         name="Turn 90 degrees",
         action_type=YawAndScan,
         action_name="/yaw_and_scan",
-        action_goal=goal_msg2
+        action_goal=goal_msg
     )
 
-    blind_push4 = py_trees_ros.action_clients.FromConstant(
+
+    # --- Second Edge (with Return Loop) ---
+    blind_push3 = py_trees_ros.action_clients.FromConstant(
         name="Blind Push Through Gate",
         action_type=BlindPush,
         action_name="/blind_push",
-        action_goal=BlindPush.Goal(
-            duration=17.0,
-            speed=0.2
-        )
+        action_goal=BlindPush.Goal(duration=17.0, speed=0.2)
     )
 
     return_loop = py_trees_ros.action_clients.FromConstant(
         name="Return Loop to Start",
         action_type=ReturnLoop,
         action_name="/return_loop",
-        action_goal=ReturnLoop.Goal(
-            duration=60.0,
-            radius=5.0
-        )
+        action_goal=ReturnLoop.Goal(duration=60.0, radius=5.0)
     )
 
-
-    goal_msg3 = YawAndScan.Goal()
-    goal_msg3.target_angle_deg = 95.0
-    goal_msg3.angular_speed = 0.05 
-
+    # ------------------------------------------
+    # Action: Rotate 90 Deg 2
+    # ------------------------------------------
     rotate_90_deg2 = py_trees_ros.action_clients.FromConstant(
         name="Turn 90 degrees",
         action_type=YawAndScan,
         action_name="/yaw_and_scan",
-        action_goal=goal_msg3
+        action_goal=goal_msg
     )
 
-    blind_push5 = py_trees_ros.action_clients.FromConstant(
+
+    # --- Third Edge ---
+    blind_push4 = py_trees_ros.action_clients.FromConstant(
         name="Blind Push Through Gate",
         action_type=BlindPush,
         action_name="/blind_push",
-        action_goal=BlindPush.Goal(
-            duration=17.0,
-            speed=0.2
-        )
+        action_goal=BlindPush.Goal(duration=17.0, speed=0.2)
     )
 
+    # ------------------------------------------
+    # Action: Rotate 90 Deg 3
+    # ------------------------------------------
     rotate_90_deg3 = py_trees_ros.action_clients.FromConstant(
         name="Turn 90 degrees",
         action_type=YawAndScan,
         action_name="/yaw_and_scan",
-        action_goal=goal_msg2
+        action_goal=goal_msg
     )
 
-    blind_push6 = py_trees_ros.action_clients.FromConstant(
+
+    # --- Fourth Edge ---
+    
+    blind_push5 = py_trees_ros.action_clients.FromConstant(
         name="Blind Push Through Gate",
         action_type=BlindPush,
         action_name="/blind_push",
-        action_goal=BlindPush.Goal(
-            duration=18.0,
-            speed=0.2
-        )
+        action_goal=BlindPush.Goal(duration=17.0, speed=0.2)
     )
 
+    # ------------------------------------------
+    # Action + Check: Switch to MANUAL 2
+    # ------------------------------------------
     mode_request_manual2 = SetVehicleMode.Request()
     mode_request_manual2.mode_name = "MANUAL"
     switch_mode_manual2 = py_trees_ros.service_clients.FromConstant(
-            name="SwitchToManual",
-            service_type=SetVehicleMode,
-            service_name="/change_mode",
-            service_request=mode_request_manual2
-        )
+        name="SwitchToManual",
+        service_type=SetVehicleMode,
+        service_name="/change_mode",
+        service_request=mode_request_manual2
+    )
 
+    wait_mode_manual2 = py_trees.decorators.Timeout(
+        name="Timeout Wait Mode MANUAL 2",
+        duration=2.0,
+        child=py_trees.behaviours.WaitForBlackboardVariableValue(
+            name="Wait Mode MANUAL 2",
+            check=py_trees.common.ComparisonExpression(
+                variable="vehicle_mode",
+                value="MANUAL",
+                operator=operator.eq)
+        )
+    )
+    
+    retry_switch_manual2 = py_trees.composites.Sequence(
+        name="Seq Switch Manual 2",
+        memory=True,
+        children=[switch_mode_manual2, wait_mode_manual2]
+    )
+
+    retry_switch_manual2 = py_trees.decorators.Retry(
+        name="Retry Switch Manual2",
+        child=retry_switch_manual2,
+        num_failures=10
+    )
+
+    # ==========================================
+    # 5. THIRD PART (FINAL RUN)
+    # ==========================================
     wait_60_secs3 = py_trees.timers.Timer(name="Wait 60 Seconds", duration=20.0)
 
+    # ------------------------------------------
+    # Action + Check: Switch to ALT_HOLD 3
+    # ------------------------------------------
     mode_request_althold3 = SetVehicleMode.Request()
     mode_request_althold3.mode_name = "ALT_HOLD"
     switch_mode_althold3 = py_trees_ros.service_clients.FromConstant(
@@ -247,47 +355,66 @@ def create_root() -> py_trees.behaviour.Behaviour:
         service_type=SetVehicleMode,
         service_name="/change_mode",
         service_request=mode_request_althold3
-        )
+    )
 
-    blind_push7 = py_trees_ros.action_clients.FromConstant(
-        name="Blind Push Through Gate",
-        action_type=BlindPush,
-        action_name="/blind_push",
-        action_goal=BlindPush.Goal(
-            duration=40.0,
-            speed=0.2
+    wait_mode_althold3 = py_trees.decorators.Timeout(
+        name="Timeout Wait Mode ALT_HOLD 3",
+        duration=2.0,
+        child=py_trees.behaviours.WaitForBlackboardVariableValue(
+            name="Wait Mode ALT_HOLD 3",
+            check=py_trees.common.ComparisonExpression(
+                variable="vehicle_mode",
+                value="ALT_HOLD",
+                operator=operator.eq)
         )
     )
 
-    arrange_depth_sequence.add_children([
+    retry_switch_althold3 = py_trees.composites.Sequence(
+        name="Seq Switch AltHold 3",
+        memory=True,
+        children=[switch_mode_althold3, wait_mode_althold3]
+    )
+
+    retry_switch_althold3 = py_trees.decorators.Retry(
+        name="Retry Switch Althold3",
+        child=retry_switch_althold3,
+        num_failures=10
+    )
+
+    # ------------------------------------------
+    # Action: Blind Push 6
+    # ------------------------------------------
+    blind_push6 = py_trees_ros.action_clients.FromConstant(
+        name="Blind Push Through Gate",
+        action_type=BlindPush,
+        action_name="/blind_push",
+        action_goal=BlindPush.Goal(duration=40.0, speed=0.2)
+    )
+
+    # ==========================================
+    # 6. ASSEMBLE MAIN TREE
+    # ==========================================
+    main_mission_sequence.add_children([
         wait_60_secs1,
-        switch_mode_althold1,
+        retry_switch_althold1,
         arrange_depth1,
         blind_push1,
-        rotate_180_deg,
-        blind_push2,
-        switch_mode_manual1,
+        retry_switch_manual1,
         wait_60_secs2,
-        switch_mode_althold2,
+        retry_switch_althold2,
         arrange_depth2,
-        blind_push3,
+        blind_push2,
         rotate_90_deg1,
-        blind_push4,
-        return_loop,
+        blind_push3, 
+        return_loop, 
         rotate_90_deg2,
-        blind_push5,
+        blind_push4, 
         rotate_90_deg3,
-        blind_push6,
-        switch_mode_manual2,
+        blind_push5,
+        retry_switch_manual2,
         wait_60_secs3,
-        # switch_mode_althold3,
-        blind_push7,
-
-    ])
-
-
-    main_mission_sequence.add_children([
-        arrange_depth_sequence,
+        retry_switch_althold3, 
+        blind_push6
     ])
 
     root.add_child(publishers_parallel)
